3fd5dadaa7e3c6c43bfff6478da46496ccb38bc6
poece <matthew98981@gmail.com>
2015-03-09 00:58:03 +0800

    201539

--- app/src/main/java/p/android_chapter_11/MainActivity.java
+++ app/src/main/java/p/android_chapter_11/MainActivity.java
@@ -2,6 +2,8 @@ package p.android_chapter_11;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,48 +13,100 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+import java.security.MessageDigestSpi;
 
 
 public class MainActivity extends Activity {
     View view;
-    Handler handler = new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(Message msg) {
-            if (msg.what == 0) {
-                Thread thread1 = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        while (true) {
-                            try {
-                                Thread.sleep(10);
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                            Log.i("333333333333", "dssssssssssssssss");
-                            view.postInvalidate();
-                        }
-                    }
-                });
-                thread1.start();
-            }
-            return false;
-        }
-    });
+    ImageView imageView;
+    ImageView small_imageView;
+    AnimationDrawable animationDrawable;
+    Thread controlFrame;
+//    Handler handler = new Handler(new Handler.Callback() {
+//        @Override
+//        public boolean handleMessage(Message msg) {   //运动的view
+//            if (msg.arg2 == 2) {
+//                Thread thread1 = new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        while (true) {
+////                            try {
+////                                Thread.sleep(5);
+////                            } catch (InterruptedException e) {
+////                                e.printStackTrace();
+////                            }
+//                            Log.i("333333333333", "dssssssssssssssss");
+//                            view.postInvalidate();
+//                        }
+//                    }
+//                });
+//                thread1.start();
+//            }
+//            return false;
+//        }
+//    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        view = new myView(this);
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                while(true) {
-//                    view.postInvalidate();
-//                }
-//            }
-//        }).start();
-        handler.sendEmptyMessage(0);
-        setContentView(view);
+
+//        LinearLayout linearLayout = new LinearLayout(this);
+//        linearLayout.setOrientation(LinearLayout.VERTICAL);
+//        setContentView(linearLayout);
+//        Button button = new Button(this);
+//        button.setText("create by code");
+//        button.setId(1);
+//        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup
+//                .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        layoutParams.setMargins(100, 0, 0, 0);
+//        linearLayout.addView(button, layoutParams);
+//
+//        ImageView imageView1 = new ImageView(this);
+//        imageView1.setImageResource(R.anim.m_anim);
+//        animationDrawable = (AnimationDrawable) imageView1.getBackground();
+//        LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(ViewGroup
+//                .LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+//        layoutParams1.setMargins(100, 100, 0, 0);
+//        linearLayout.addView(imageView1, layoutParams1);
+
+//        view = new myView(this);
+//       setContentView(view);
+        setContentView(R.layout.activity_main);
+//
+////        new Thread(new Runnable() {
+////            @Override
+////            public void run() {
+////                while(true) {
+////                    view.postInvalidate();
+////                }
+////            }
+////        }).start();
+////        Message message=new Message();
+////        message.arg1=1;
+////        message.arg2=2;
+////        message.what=3;
+////        message.obj= new Thread();
+////        handler.sendMessage(message);
+////        setContentView(view);
+        imageView = (ImageView) findViewById(R.id.myImageview);
+        small_imageView = (ImageView) findViewById(R.id.small_image);
+        imageView.setBackgroundResource(R.anim.m_anim);
+        animationDrawable = (AnimationDrawable) imageView.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {  //必须放在这里才能运行
+        super.onWindowFocusChanged(hasFocus);
+//        imageView= (ImageView) findViewById(R.id.myImageview);
+//        imageView.setBackgroundResource(R.anim.m_anim);
+//        animationDrawable=(AnimationDrawable)imageView.getBackground();
+//        animationDrawable.start();
 
     }
 
@@ -77,5 +131,53 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void start(View view) {
+        if (!animationDrawable.isRunning()) {
+            animationDrawable.start();
+            controlFrame = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Field field = AnimationDrawable.class.getDeclaredField("mCurFrame");
+                        field.setAccessible(true);
+                        while (true) {
+                            int currentFrame = field.getInt(animationDrawable);
+                            if (currentFrame == 2 || currentFrame == 3) {
+                                field.setInt(animationDrawable, 4);
+                            }
+                        }
+                    } catch (Exception e) {
+                        Log.e("ERROR", e.getMessage());
+                    }
+                }
+            });
+            controlFrame.start();
+        } else {
+            try {
+                controlFrame.interrupt();
+
+            } catch (Exception e) {
+                Log.i("Thread Stop Exception", e.getMessage());
+            }
+            animationDrawable.stop();
+////            int number = animationDrawable.getNumberOfFrames();
+////            int duration = animationDrawable.getDuration(1);
+//            Drawable drawable = animationDrawable.getCurrent();
+//            int len = animationDrawable.getNumberOfFrames();
+//            long currentTime1=System.nanoTime();
+//            for (int i = 0; i < len; i++) {   //7ms ~ 12ms 运行时间
+//                if (animationDrawable.getFrame(i).equals(animationDrawable.getCurrent())) {
+//                    Toast.makeText(this, i+"", Toast.LENGTH_LONG).show();
+//                    small_imageView.setImageDrawable(animationDrawable.getCurrent());
+//                }
+//            }
+//            long currentTime2=System.nanoTime();
+//            Log.i("eclipseTime",currentTime2-currentTime1+"");
+////            small_imageView.setImageDrawable(drawable);
+////            Toast.makeText(this, number + " " + duration, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- app/src/main/java/p/android_chapter_11/myView.java
+++ app/src/main/java/p/android_chapter_11/myView.java
@@ -5,24 +5,41 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ComposePathEffect;
+import android.graphics.CornerPathEffect;
+import android.graphics.DashPathEffect;
+import android.graphics.DiscretePathEffect;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.text.Layout;
 import android.text.StaticLayout;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import java.io.InputStream;
+
+import javax.xml.validation.Validator;
 
 /**
  * Created by wangxm-wr on 2015/3/6 in Vanke.
  */
 public class myView extends View {
+    Context context;
+
     public myView(Context context) {
         super(context);
+        this.context = context;
     }
 
     public myView(Context context, AttributeSet attrs) {
@@ -34,10 +51,15 @@ public class myView extends View {
     }
 
     static int degree = 1;
+    static int offset = 1;
+
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
 //        Paint paint1 = new Paint();
 //        paint1.setColor(Color.RED);
 //        paint1.setStrokeWidth(10);
@@ -46,23 +68,23 @@ public class myView extends View {
 //        paint1.setStyle(Paint.Style.FILL);
 //        canvas.drawLine(20, 20, 200, 230, paint1);
 //
-        Paint paint2 = new Paint();
-        paint2.setColor(Color.GRAY);
-        paint2.setStrokeWidth(3);
-        paint2.setAntiAlias(true);
-        paint2.setDither(true);
-        paint2.setTextSize(40);
-        paint2.setStyle(Paint.Style.STROKE);
+//        Paint paint2 = new Paint();
+//        paint2.setColor(Color.GRAY);
+//        paint2.setStrokeWidth(3);
+//        paint2.setAntiAlias(true);
+//        paint2.setDither(true);
+//        paint2.setTextSize(40);
+//        paint2.setStyle(Paint.Style.STROKE);
 //        float[] points2 = {1.21f, 3.32f, 12, 42, 83, 211};
 //        canvas.drawPoints(points2,0,6,paint2);
-
-        Paint paint3 = new Paint();
-        paint3.setColor(Color.RED);
-        paint3.setStrokeWidth(1);
-        paint3.setAntiAlias(true);
-        paint3.setDither(true);
-        paint3.setTextSize(40);
-        paint3.setStyle(Paint.Style.STROKE);
+//
+//        Paint paint3 = new Paint();
+//        paint3.setColor(Color.RED);
+//        paint3.setStrokeWidth(3);
+//        paint3.setAntiAlias(true);
+//        paint3.setDither(true);
+//        paint3.setTextSize(40);
+//        paint3.setStyle(Paint.Style.STROKE);
 //        float[] points3 = {200, 200, 100, 400, 500, 800};   //画三角形
 ////        float[] points3 = {200, 200, 100, 400, 500, 800,200,200};
 ////        canvas.drawLines(points3,0,4,paint3);
@@ -104,12 +126,98 @@ public class myView extends View {
 //        matrix.setRotate(20,20,20);
 //        canvas.setMatrix(matrix);
 
-        Log.i("dsfsdf", "111111111111111111111111111");
-        degree += 1;            //显示动画
-        canvas.rotate(degree, 310, 310);
-        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pic), 300, 300, paint3);
+//        Log.i("dsfsdf", "111111111111111111111111111");
+//        degree += 1;            //显示动画
+//        canvas.rotate(degree, 310, 310);
+//        canvas.drawBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.pic), 300, 300, paint3);
 //        invalidate();
 //        Log.i("dsfsdf", "222222222222222222222");
+//        offset+=20;
+//
+//        DashPathEffect pathEffect = new DashPathEffect(new float[]{
+//                40, 5, 20, 5
+//        }, offset);
+//        CornerPathEffect cornerPathEffect=new CornerPathEffect(50);
+//
+//        Path p=new Path();
+//        p.addCircle(300,300,100, Path.Direction.CCW);
+//
+//        PathDashPathEffect pDP=new PathDashPathEffect(p,1,offset, PathDashPathEffect.Style.TRANSLATE);
+//        Paint paint4 = new Paint();
+//        paint4.setPathEffect(new ComposePathEffect(pathEffect,cornerPathEffect));
+//        paint4.setColor(Color.RED);
+//        paint4.setStyle(Paint.Style.STROKE);
+//        paint4.setStrokeWidth(5);
+//        Path path = new Path();
+//        path.moveTo(10, 10);
+//
+//        path.lineTo(300, 344);
+//        path.lineTo(530, 144);
+
+//        Paint paint5=new Paint();
+//        paint5.setStrokeWidth(3);
+//        paint5.setColor(Color.RED);
+//        paint5.setStyle(Paint.Style.STROKE);
+//        paint5.setPathEffect(new ComposePathEffect(pDP,cornerPathEffect));
+//
+//canvas.drawColor(Color.WHITE);
+//        canvas.drawPath(p, paint5);
+//        try {
+//            Thread.sleep(200);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        invalidate();
+
+//        offset += 20;
+//        Paint paint = new Paint();
+//        paint.setStrokeWidth(2);
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setColor(Color.RED);
+//        paint.setAntiAlias(true);
+//        paint.setDither(true);
+//
+//        Path path = new Path();
+//        path.moveTo(80, 800);
+////        for (int i = 2; i < 8; i++) {
+////            path.lineTo(80 * i, (float) Math.random() * 800);
+////        }
+//        path.lineTo(160,400);
+//        path.lineTo(240,800);
+//
+//        Path path1 = new Path();
+//        path1.moveTo(180, 800);
+//        path1.addRect(100, 100, 321, 324, Path.Direction.CCW);
+//
+//
+//        PathEffect[] pE = new PathEffect[7];
+//        pE[0] = null;
+//        pE[1] = new CornerPathEffect(50);
+//        pE[2] = new DiscretePathEffect(1, 5);
+//        pE[3] = new DashPathEffect(new float[]{20, 10, 15, 3}, offset);
+//        pE[4] = new PathDashPathEffect(path1, 1, offset, PathDashPathEffect.Style.ROTATE);
+//
+////        paint.setPathEffect(new ComposePathEffect(pE[3],pE[1]));
+//        paint.setPathEffect(pE[4]);
+//        canvas.drawPath(path, paint);
+//        invalidate();
+
+//        Paint paint = new Paint();
+//        paint.setAntiAlias(true);
+//        paint.setStrokeWidth(3);
+//        paint.setTextSize(30);
+//        paint.setColor(Color.RED);
+//        paint.setTextAlign(Paint.Align.RIGHT);
+//
+//        Path path = new Path();
+//        path.addCircle(300, 300, 200, Path.Direction.CW);
+//
+//        canvas.drawColor(Color.WHITE);
+////        canvas.drawTextOnPath("ANDROID DESIGN", path, 0, 0, paint);
+////        canvas.translate(100,100);
+//        canvas.drawTextOnPath("ANDROID DESIGN ANDROID DESIGN ANDROID DESIGN", path, 0, 0, paint);
+
 
     }
+
 }
